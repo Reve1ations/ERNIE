9648136d9262a11d3f1bab1b3e2f3d8db9ea378c
zhouzhanzhao123 <2115516135@qq.com>
2020-07-01 21:01:59 +0800

    rescore_classify

--- script/zh_task/ernie_base/run_ChnSentiCorp.sh
+++ script/zh_task/ernie_base/run_ChnSentiCorp.sh
@@ -11,20 +11,20 @@ python -u ./ernie/run_classifier.py \
                    --do_train true \
                    --do_val true \
                    --do_test true \
-                   --batch_size 32 \
-                   --init_pretraining_params /ernie_model/params \
-                   --train_set ./classified_data/train.txt \
-                   --dev_set ./classified_data/dev.txt \
-                   --test_set ./classified_data/test.txt \
-                   --vocab_path /ernie_model/vocab.txt \
+                   --batch_size 16 \
+                   --init_pretraining_params /ERNIE_model/params \
+                   --train_set ./dataset/rescore/classify/train_classify.txt \
+                   --dev_set ./dataset/rescore/classify/dev_classify.txt \
+                   --test_set ./dataset/rescore/classify/test_classify.txt \
+                   --vocab_path /ERNIE_model/vocab.txt \
                    --checkpoints ./checkpoints \
                    --save_steps 1000 \
                    --weight_decay  0.01 \
                    --warmup_proportion 0.0 \
                    --validation_steps 100 \
                    --epoch 10 \
-                   --max_seq_len 128 \
-                   --ernie_config_path /ernie_model/ernie_config.json \
+                   --max_seq_len 512 \
+                   --ernie_config_path /ERNIE_model/ernie_config.json \
                    --learning_rate 5e-5 \
                    --skip_steps 10 \
                    --num_iteration_per_drop_scope 1 \
