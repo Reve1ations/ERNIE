46e9d77d27b293536ec331ed5d1b207dfa6f870f
zhouzhanzhao123 <2115516135@qq.com>
2020-07-10 10:45:43 +0800

    score_origin

--- script/zh_task/ernie_base/run_drcd.sh
+++ script/zh_task/ernie_base/run_drcd.sh
@@ -26,9 +26,9 @@ python ./ernie/run_mrc.py --use_cuda true\
                     --max_answer_length 30 \
                     --do_lower_case true \
                     --doc_stride 128 \
-                  	--train_set ./dataset/decomp/first_question/train_first.json \
+                  	--train_set ./dataset/decomp/combine_final/train_single.json \
                     --dev_set ./dataset/origin/no.json \
-                  	--test_set ./dataset/score/simp_train_test_first.json \
+                  	--test_set ./dataset/score/simp_train_test.json \
                     --learning_rate 5e-5 \
                     --num_iteration_per_drop_scope 1 \
                     --init_pretraining_params /ERNIE_model/params \
